0d614032c9a1427f4e8412a028461ef83c42425f
Peter France <pfrance@gmail.com>
2014-02-05 13:25:58 -0500

    bugs

--- main.hs
+++ main.hs
@@ -7,7 +7,8 @@ import System.Environment
 import Symbol
 import qualified Data.Map as Map
 import Control.Monad.State
-
+import Data.String.Utils
+import Data.Maybe
 
 type TableState = (String, SymbolTable)
 type StateMonad = State TableState
@@ -17,7 +18,7 @@ firstPass :: Program -> SymbolTable -> SymbolTable
 firstPass prog symMap = snd $ foldl enterSymbol (0, symMap) prog
   where enterSymbol (idx, map) inst = case inst of  AInstruction s -> (idx +1, map)
                                                     CInstruction d c j -> (idx +1, map)
-                                                    Pseudo s -> (idx, Map.insert s (show idx) map)
+                                                    Pseudo s -> (idx, Map.insert s (fromJust $ aToBinary (show idx) map) map)
 
 
 type AssembleState = (String, SymbolTable, Int)
@@ -26,22 +27,12 @@ translateAndUpdate :: Instruction -> AssembleState -> AssembleState
 translateAndUpdate inst (istr, table, idx) = case inst of
                                       AInstruction s      -> case aToBinary s table of 
                                                               Just bs -> (istr ++ "\n" ++ bs, table, idx)
-                                                              Nothing ->  let   bs      = maybe' "" $ aToBinary (show idx) table
+                                                              Nothing ->  let   bs      = fromJust $ aToBinary (show idx) table
                                                                                 aTable  = Map.insert s bs table
                                                                           in (istr ++ "\n" ++ bs, aTable, idx + 1)
                                       CInstruction d c j  -> let cstr = cToBinary d c j in (istr ++ "\n" ++ cstr, table, idx) 
                                       _                   -> (istr, table, idx)
 
---assemble program = let (byteStr, sym, ind) = last $ liftM2 translateAndUpdate program (return initialState) in byteStr
---  where initialState = ("", firstPass program symbolTable, 16)
-
---secondPass :: Program -> AssembleState
---get set the result value to the state and left the state unchanged.
---put set the result value to () and set the state value.
---modify f = do { x <- get; put (f x) }
-
---secondPass :: MonadState AssembleState m => Program -> m String
---secondPass :: MonadState AssembleState m => Program -> m AssembleState
 secondPass [] = do
   st <- get
   return st
@@ -60,4 +51,5 @@ main = do
   let path = takeWhile (\x -> x /= '.') (head args)
   case parse parseProgram "" prog of
     Left err -> print err
-    Right x -> writeFile (path ++ ".hack") $ let (byteStr, sT, idx) = assemble x in byteStr
+    --Right x -> print $ firstPass x sy
+    Right x -> writeFile (path ++ ".hack") $ let (byteStr, sT, idx) = assemble x in lstrip byteStr
--- symbol.hs
+++ symbol.hs
@@ -17,3 +17,5 @@ symbolTable :: SymbolTable
 symbolTable = Map.unions [
   (Map.fromList [("SP", "0"), ("LCL", "1"), ("ARG", "10"), ("THIS", "11"),("THAT", "110"),("SCREEN", numToBin "16384"),("KBD", numToBin "24576")]),
   (Map.fromList [("R" ++ show n, numToBin $ show n) | n <- [0..15]])]
+  --where prefix v = take (16 - (length $ numToBin v)) $ repeat '0'
+--Map.map prefix 
